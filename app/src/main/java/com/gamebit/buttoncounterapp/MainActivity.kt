3053b795f949629a9a0071ceeddbfb52e686857b
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-18 21:11:46 +0300

    Build Optimized

--- app/src/main/java/com/gamebit/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/gamebit/buttoncounterapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.gamebit.buttoncounterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,13 +14,30 @@ class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
     private var button: Button? = null
     private var textView: TextView? = null
+    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
-        textView = findViewById<TextView>(R.id.textView)
+        userInput = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                Log.d("xxx","")
+                numTimesClicked += 1
+                textView?.append("The button got tapped $numTimesClicked time")
+                if (numTimesClicked != 1){
+                    textView?.append("s\n")
+                } else{
+                    textView?.append("\n")
+                }
+
+            }
+        })
     }
 }
