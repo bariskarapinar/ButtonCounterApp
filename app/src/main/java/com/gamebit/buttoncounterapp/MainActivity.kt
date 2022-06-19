66e3f6f4413a0e19ee05d3fcf512d3ff6d7f9834
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-19 13:47:17 +0300

    Button Counter App

--- app/src/main/java/com/gamebit/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/gamebit/buttoncounterapp/MainActivity.kt
@@ -2,12 +2,15 @@ package com.gamebit.buttoncounterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+
+private const val TEXT_CONTENTS = "TextContent"
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +34,15 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState.getString(TEXT_CONTENTS, "")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString(TEXT_CONTENTS, textView?.text.toString())
     }
 }
